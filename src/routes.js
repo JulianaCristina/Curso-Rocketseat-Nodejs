915d2aaa8e38f5dc7f5ae399a74bcb214e8bb3ac
juliana <julianacristina780@gmail.com>
2020-05-12 19:50:38 -0300

    CRUD

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,8 @@ const ProductController = require('./controllers/ProductController')
 //A primeira rota
 routes.get('/products', ProductController.index)
 routes.post('/products', ProductController.store)
+routes.get('/products/:id', ProductController.show)
+routes.put('/products/:id', ProductController.update)
+routes.delete('/products/:id', ProductController.destroy)
 
 module.exports = routes;
